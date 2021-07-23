4eb1dd9fbabe7a782aa2070de144240616c00472
Julius Piso <julius@piso.at>
2021-07-23 17:06:02 +0200

    Updated csv formats. Multiple simulation outputs are averaged.

--- trench_automation/main.py
+++ trench_automation/main.py
@@ -1,15 +1,16 @@
-from argparse import ArgumentParser
+import csv
+from argparse import ArgumentParser, ArgumentTypeError
 from os import path
 from string import Template
 from subprocess import Popen
 from tempfile import NamedTemporaryFile
 
 import numpy as np
-import pandas as pd
 
 import util
 
-# This only works if the directory where "generate_trench.so" is located is present in the PYTHONPATH environment variable
+# This import only works if the directory where "generate_trench.so" is located is present in
+# the PYTHONPATH environment variable
 #import generate_trench
 
 
@@ -18,89 +19,120 @@ PROJECT_DIRECTORY = path.dirname(__file__)
 PROCESS_TIME = 10
 DISTANCE_BITS = 8
 OUTPUT_DIR = path.join(PROJECT_DIRECTORY, "output")
-N = 12
-
 
 parser = ArgumentParser(
-    description='Run physical deposition simulations with different sticking probabilities.')
-parser.add_argument('CSV',
-                    type=str,
-                    default="results.csv",
-                    nargs="?",
-                    help='CSV file for saving the results')
+    description="Run physical deposition simulations with different sticking probabilities.")
+parser.add_argument(
+    "output",
+    type=str,
+    default="results.csv",
+    nargs="?",
+    help="output CSV file for saving the results")
+
+
+def check_list_input(x):
+    """ Converts the input string to a list of floats. Only uses input elements with a value between 0 and 1."""
+    x = x.replace("[", "").replace("]", "").split(",")
+    try:
+        x = [float(i) for i in x]
+    except ValueError as e:
+        raise ArgumentTypeError(e)
+
+    if np.all([0 < i <= 1 for i in x]):
+        if len(x) == 0:
+            raise ArgumentTypeError("No sticking probability values provided")
+        return x
+    else:
+        raise ArgumentTypeError(
+            "The sticking probability has to have a value between 0 and 1.")
+
+
+parser.add_argument(
+    "--sticking-probabilities",
+    dest="sticking_probabilities",
+    type=check_list_input,
+    default=[1/2**i for i in range(5)],
+    help="list of sticking probabilities to be used during the simulation"
+)
+
+parser.add_argument(
+    "--repetitions",
+    dest="repetitions",
+    type=int,
+    default=10,
+    help="how often the simulation should be repeated for one set of parameters")
 
 
 def main():
     args = parser.parse_args()
-
     # Read the template file into a string variable
     with open(path.join(PROJECT_DIRECTORY, "parameters.template"), "r") as f:
         template_string = f.read()
 
-    tx, ty = None, None
+    # Enforce csv file ending and generate additional filename for csv file for saving the geometry
+    basename = path.splitext(args.output)[0]
+    data_fname = basename + ".csv"
+    geometry_fname = basename + "_geom.csv"
 
-    deposition_thickness = []
+    # Open the files and create csv writers for them
+    with open(data_fname, "w+") as datafile, open(geometry_fname, "w+") as geomfile:
+        data_writer = csv.writer(datafile)
+        geometry_writer = csv.writer(geomfile)
 
-    for i in range(N+1):
-        sticking_probability = 1/2**i
-        print(f"Sticking probability: {sticking_probability}")
-        # Use the template to create the content of the parameter file
-        s = Template(template_string)
-        out = s.substitute(
-            GEOMETRY_FILE=path.join(PROJECT_DIRECTORY, "trench.vtk"),
-            DISTANCE_BITS=DISTANCE_BITS,
-            OUTPUT_PATH=OUTPUT_DIR,  # path.join(OUTPUT_DIR, f"result_{i}"),
-            FD_SCHEME="LAX_FRIEDRICHS_1ST_ORDER",
-            PROCESS_TIME=PROCESS_TIME,
-            # ",".join([str(i) for i in range(11)]),
-            OUTPUT_VOLUME=PROCESS_TIME,
-            DEPOSITION_RATE="1.",
-            STICKING_PROBABILITY=sticking_probability,
-            STATISTICAL_ACCURACY="1000.")
+        # Here we could generate new trench geometries using the generate_trench module...
 
-        # Create a temporary file with the content we just generated
-        # which can be used as an input for ViennaTS
-        paramfile = NamedTemporaryFile(mode='w+')
-        paramfile.file.write(out)
-        paramfile.file.flush()
+        tx, ty = None, None
+        geometry_id = -1
 
-        # Call ViennaTS with the just generated temporary process definition file
-        Popen([VIENNATS_EXE, paramfile.name],
-              cwd=PROJECT_DIRECTORY).wait()
+        for sticking_probability in args.sticking_probabilities:
+            print(f"Sticking probability: {sticking_probability}")
+            # Use the template to create the content of the parameter file
+            s = Template(template_string)
+            out = s.substitute(
+                GEOMETRY_FILE=path.join(PROJECT_DIRECTORY, "trench.vtk"),
+                DISTANCE_BITS=DISTANCE_BITS,
+                # path.join(OUTPUT_DIR, f"result_{i}"),
+                OUTPUT_PATH=OUTPUT_DIR,
+                FD_SCHEME="LAX_FRIEDRICHS_1ST_ORDER",
+                PROCESS_TIME=PROCESS_TIME,
+                # ",".join([str(i) for i in range(11)]),
+                OUTPUT_VOLUME=PROCESS_TIME,
+                DEPOSITION_RATE="1.",
+                STICKING_PROBABILITY=sticking_probability,
+                STATISTICAL_ACCURACY="1000.")
 
-        # Close/ Destroy the tempfile
-        paramfile.close()
+            # Create a temporary file with the content we just generated
+            # which can be used as an input for ViennaTS
+            with NamedTemporaryFile(mode="w+") as paramfile:
+                paramfile.file.write(out)
+                paramfile.file.flush()
 
-        # Load the points along the trench surface, if they aren't already loaded
-        if tx is None:
-            tx, ty, _ = util.extract_line(
-                path.join(OUTPUT_DIR + f"_{DISTANCE_BITS}bit", "Interface_0_0.vtp"))
+                for _ in range(args.repetitions):
+                    # Call ViennaTS with the just generated temporary process definition file
+                    Popen([VIENNATS_EXE, paramfile.name],
+                          cwd=PROJECT_DIRECTORY).wait()
 
-        # Load the points along the surface of the deposited layer
-        x, y, _ = util.extract_line(
-            path.join(OUTPUT_DIR + f"_{DISTANCE_BITS}bit", "Interface_1_0.vtp"))
+                    # Load the points along the trench surface, if they aren't already loaded
+                    if tx is None:
+                        tx, ty, _ = util.extract_line(
+                            path.join(OUTPUT_DIR + f"_{DISTANCE_BITS}bit", "Interface_0_0.vtp"))
+                        geometry_id = geometry_id + 1
+                        geometry_writer.writerow(
+                            [geometry_id, 0] + tx.flatten().tolist())
+                        geometry_writer.writerow(
+                            [geometry_id, 1] + ty.flatten().tolist())
 
-        # Calculate the layer thickness
-        dist = util.line_to_distance(tx, ty, x, y)
-        # Add the layer thickness to the array, but first append the current sticking probability to them
-        deposition_thickness.append(np.insert(dist, 0, sticking_probability))
+                    # Load the points along the surface of the deposited layer
+                    x, y, _ = util.extract_line(
+                        path.join(OUTPUT_DIR + f"_{DISTANCE_BITS}bit", "Interface_1_0.vtp"))
 
-    # Add the x and y coordinates of the trench geometry to the array
-    deposition_thickness.insert(0, np.insert(ty, 0, -1))
-    deposition_thickness.insert(0, np.insert(tx, 0, -1))
+                    # Calculate the layer thickness
+                    dist = util.line_to_distance(tx, ty, x, y)
 
-    # Convert the results into a pandas DataFrame
-    df = pd.DataFrame(deposition_thickness)
-    # Set the sticking probability as the index
-    df.index = df[0]
-    df.index.rename("sticking_probability", inplace=True)
-    df.drop(0, inplace=True, axis=1)
-
-    # Print the DataFrame
-    print(df)
-
-    # Save the DataFrame as a csv file
-    df.to_csv(args.CSV, index=True, header=True)
+                    # Add the layer thickness to the array, but first append the current geometry_id and
+                    # sticking probability to them
+                    data_writer.writerow([geometry_id, sticking_probability] +
+                                         dist.flatten().tolist())
 
     print("Done!")
 
--- trench_automation/visualize.py
+++ trench_automation/visualize.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 parser = argparse.ArgumentParser(
     description='Visualize interface lines of physical deposition models with different sticking probabilities.')
-parser.add_argument('csv',
+parser.add_argument('input',
                     type=str,
                     help='CSV input file')
 
@@ -15,26 +15,54 @@ parser.add_argument('csv',
 def main():
     # Parse the command line arguments
     args = parser.parse_args()
-    df = pd.read_csv(args.csv, index_col=0)
+    # Enforce csv file ending and generate additional filename for csv file for saving the geometry
+    basename = path.splitext(args.input)[0]
+    data_fname = basename + ".csv"
+    geometry_fname = basename + "_geom.csv"
 
-    # Extract the geometry rows
-    tx = df.iloc[0].to_numpy()
-    ty = df.iloc[1].to_numpy()
+    data_df = pd.read_csv(data_fname, header=None)
+    data_df.columns = ["geometry_id",
+                       "sticking_probability"] + data_df.columns[:-2].to_list()
+    geom_df = pd.read_csv(geometry_fname, header=None)
+    geom_df.columns = ["geometry_id", "axis"] + geom_df.columns[:-2].to_list()
 
-    # Remove the geometry rows
-    df = df.iloc[2:]
+    for geometry_id in geom_df["geometry_id"].unique():
+        tx = geom_df[(geom_df["geometry_id"] == geometry_id) & (
+            geom_df["axis"] == 0)].drop(["geometry_id", "axis"], axis=1).to_numpy()[0]
+        ty = geom_df[(geom_df["geometry_id"] == geometry_id) & (
+            geom_df["axis"] == 1)].drop(["geometry_id", "axis"], axis=1).to_numpy()[0]
 
-    # Mask off all elements except those contributing to the left sidewall
-    sidewall_mask = (tx < 0) & (ty < -0.5) & (ty > np.min(ty) + 0.5)
+        df = data_df[data_df["geometry_id"] == geometry_id].drop(
+            ["geometry_id"], axis=1).groupby("sticking_probability").mean()
 
-    # Plot the results
-    for s, dist in df.iterrows():
-        plt.plot(-ty[sidewall_mask], dist[sidewall_mask],
-                 label=f"s={s}")  # "$s=2^{-"+str(s)+"}$")
-    plt.xlabel("y position [nm]")
-    plt.ylabel("Deposition thickness [nm]")
-    plt.legend()
+        print(df)
+        print(tx, ty)
+
+        # Mask off all elements except those contributing to the left sidewall
+        sidewall_mask = (tx < 0) & (ty < -0.5) & (ty > np.min(ty) + 0.5)
+
+        # Plot the results
+        for s, dist in df.iterrows():
+            plt.plot(-ty[sidewall_mask], dist[sidewall_mask],
+                     label=f"s={s}")  # "$s=2^{-"+str(s)+"}$")
+        plt.xlabel("y position [nm]")
+        plt.ylabel("Deposition thickness [nm]")
+        plt.legend()
+        plt.show()
+
+    """
+    fig, ax = plt.subplots(figsize=(9,16))
+    ax.plot(tx, ty)
+
+    for mx, my, r in zip(tx, ty, df.iloc[0]):
+        ax.add_patch(plt.Circle((mx, my), r, color='b', fill=False, clip_on=True))
+    # Label plot
+    ax.set_xlabel("x [nm]")
+    ax.set_ylabel("y [nm]")
+    ax.legend(loc="upper center")
+    plt.axis("scaled")
     plt.show()
+    """
 
 
 if __name__ == "__main__":
